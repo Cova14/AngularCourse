465e27320adbd5d0d1a654b97315c94aa99d2114
Cova14 <covaslender@gmail.com>
2020-08-17 00:48:05 -0500

    Created component for heroes searched and maked it work

---
+++ 02-spa/src/app/components/search-heroes/search-heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search-heroes',
+  templateUrl: './search-heroes.component.html',
+  styles: [
+  ]
+})
+export class SearchHeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+  termino: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscaHeroes(params['termino']);
+    });
+  }
+
+  verHeroe( idx: number) {
+    this.router.navigate( ['/heroe', idx] );
+  }
+
+}
