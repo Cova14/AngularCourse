6bdbe28591544f9b26176c446fe717cc40304a2b
Cova14 <covaslender@gmail.com>
2020-08-17 01:22:24 -0500

    Using input y output

--- 02-spa/src/app/components/search-heroes/search-heroes.component.ts
+++ 02-spa/src/app/components/search-heroes/search-heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
