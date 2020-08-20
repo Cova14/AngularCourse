43c5e29a9e58e589e5ad208c3ca614116356346d
Cova14 <covaslender@gmail.com>
2020-08-20 00:28:11 -0500

    Error handling on observables

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -11,13 +11,20 @@ export class HomeComponent implements OnInit {
 
   newReleases: any[] = [];
   loading: boolean;
+  error: boolean;
+  errorMessage: string;
 
   constructor(private spotify: SpotifyService) {
+    this.error = false;
     this.loading = true
     this.spotify.getNewReleases()
       .subscribe((data:any) => {
         this.newReleases = data;
         this.loading = false;
+      }, err => {
+        this.error = true;
+        this.loading = false;
+        this.errorMessage = err.error.error.message;
       })
   }
 
