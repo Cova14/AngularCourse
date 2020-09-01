0946c1e8b42e9ed2c7ce48af0af2b218c4c7df3f
Cova14 <covaslender@gmail.com>
2020-09-01 11:59:46 -0500

    Created app, making routes and starting to use Auth0

---
+++ 07-authapp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'protected', component: ProtectedComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
