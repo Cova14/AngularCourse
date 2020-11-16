a00f37edfd990bc31f78f46b5ad57f8d16e21e0a
Cova14 <covaslender@gmail.com>
2020-11-16 14:25:32 -0600

    Starting bonus login app

---
+++ 07-bonus-loginapp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.models';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = 'arty1498@hotmail.com';
+  }
+
+
+}
